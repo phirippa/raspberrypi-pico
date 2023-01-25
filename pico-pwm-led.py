c59725fe4410f68dcf7000c0075f8b14e21776e7
김성필(Sungpil Kim) <phirippa@naver.com>
2023-01-25 23:39:30 +0900

    Update pico-pwm-led.py

--- pico-pwm-led.py
+++ pico-pwm-led.py
@@ -5,7 +5,7 @@ pwm_pin = PWM( Pin(15) )
 pwm_pin.freq(1000) # PWM Signal의 주파수는 1kHz, 따라서 주기 T = 1ms
 
 while True:
-    for duty in range(65025):
+    for duty in range(65025):       # 2^16 - 1 즉, 65535까지 아닐까?
         pwm_pin.duty_u16(duty)
         sleep(0.0001)
     for duty in range(65025, 0, -1):
